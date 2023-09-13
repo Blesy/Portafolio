6b42746625ba3c7afa5820754203662a35430db5
Blesy <blesyangulo@gmail.com>
2023-09-13 02:05:56 -0700

    vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import astroI18next from "astro-i18next";
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), mdx(), astroI18next()],
+	output: 'server',
+  	adapter: vercel({
+    	analytics: true,
+  }),
 });
