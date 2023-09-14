b5606f1b156b0a132140a8c5fdb0b64c7eb65a93
Blesy <blesyangulo@gmail.com>
2023-09-14 15:34:45 -0700

    google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import astroI18next from "astro-i18next";
+import partytown from "@astrojs/partytown";
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), mdx(), astroI18next()],
+	integrations: [tailwind(), mdx(), astroI18next(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
